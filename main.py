dd6b8f89d503da0dcd93597da78844b1b9e647cf
SMurphen <sam_murph@outlook.com>
2024-04-21 21:54:24 +1000

    adding main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,51 @@
+def main():
+    book_path = "github.com/SMurphen/bookbot/books/frankenstein.txt"
+    book_title = book_path.split("/")[-1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_title} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def sort_on(d):
+    return d["num"]
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowercased = c.lower()
+        if lowercased in chars:
+            chars[lowercased] += 1
+        else:
+            chars[lowercased] = 1
+    return chars
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+main()
